6eda850f7a062f0ea5aba23a81655c995e158fca
EdwinZhan <ed_zhan@outlook.com>
2025-03-21 15:12:33 -0400

    [Feature] Introduce WASM and Rust

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
+import wasmPack from '@wasm-tool/wasm-pack-plugin'
+import wasm from 'vite-plugin-wasm'
 
 
 // https://vite.dev/config/
@@ -9,7 +11,15 @@ export default defineConfig({
   plugins: [
       react(),
       tailwindcss(),
+      wasm(),
+      wasmPack({
+          crateDirectory: './thumbnail-wasm', // Rust 项目路径
+          outDir: './src/wasm' // 输出目录
+      })
   ],
+    optimizeDeps: {
+        exclude: ['@wasm-tool/wasm-pack-plugin']
+    },
     //set alias of src folder
     resolve: {
         alias: {
