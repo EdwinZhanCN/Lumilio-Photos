23cc11a9203931b6cfeb6693253ce8a775086dd5
EdwinZhan <ed_zhan@outlook.com>
2025-04-19 05:06:34 -0400

    Update WASM Packs

--- web/src/wasm/thumbnail_wasm.d.ts
+++ web/src/wasm/thumbnail_wasm.d.ts
@@ -1,19 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-
-/**
- * Declares the default init function, which your code calls as "init()".
- */
-export default function init(): Promise<void>;
-
-/**
- * Declares the generate_thumbnail function from the WASM.
- */
 export function generate_thumbnail(buffer: Uint8Array, max_size: number): Uint8Array;
-
-/**
- * Any other classes or types remain as before.
- */
 export class ThumbnailResult {
   private constructor();
   free(): void;
@@ -21,3 +8,40 @@ export class ThumbnailResult {
   readonly height: number;
   readonly data: Uint8Array;
 }
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_thumbnailresult_free: (a: number, b: number) => void;
+  readonly thumbnailresult_width: (a: number) => number;
+  readonly thumbnailresult_height: (a: number) => number;
+  readonly thumbnailresult_data: (a: number) => [number, number];
+  readonly generate_thumbnail: (a: number, b: number, c: number) => [number, number, number, number];
+  readonly __wbindgen_export_0: WebAssembly.Table;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __externref_table_dealloc: (a: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
