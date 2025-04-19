1c7bb7e86cf5b90ae5e6830b6d1e40a28bdbea84
EdwinZhan <ed_zhan@outlook.com>
2025-04-19 04:54:06 -0400

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import wasm from 'vite-plugin-wasm'
 import path from "path"
+import topLevelAwait from 'vite-plugin-top-level-await'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,5 +16,7 @@ export default defineConfig({
     plugins: [
       react(),
       tailwindcss(),
+        wasm(),
+        topLevelAwait(),
     ],
 })
